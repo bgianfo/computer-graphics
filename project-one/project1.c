48e8b69fccfdec0b0e921129e9d44c4f6c8ec842
Brian Gianforcaro <b.gianfo@gmail.com>
2009-01-07 18:24:41 -0500

    Starting project A-new, it kind of sucks.

--- project-one/project1.c
+++ project-one/project1.c
@@ -1,124 +1,128 @@
-/* This example shows the effect of not enabling depth testing.  
+/*
+ * Computer Graphics - Project #1 
  *
- * The camera is in a fixed position and two spheres are drawn.
- * The second sphere (red) rotates around the first sphere (green)
- * on the y axis.
+ * Brian Gianforcaro - bjg1955@cs.rit.edu 
+ *
+ * GL_POINTS
+ * GL_LINES
+ * -DONE- At least one of GL_LINE_STRIP, GL_LINE_LOOP
+ * -DONE- At least one of GL_TRIANGLES, GL_TRIANGLE_STRIP, GL_TRIANGLE_FAN
+ * -DONE- At least one of GL_QUADS, GL_QUAD_STRIP
+ * GL_POLYGON
+ *
  */
 
 #if defined(__APPLE__) && defined(__MACH__)
-#	include <GLUT/glut.h>
+#       include <GLUT/glut.h>
 #else
 #	include <GL/glut.h>
 #endif
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
-static int depth = GL_TRUE;
-int count = 0;
-static float tree[12][3] = { {0,  0,  0}, {-.1,-.1,0}, {.1, -.1,0},
-                             {-.2,-.2,0}, {0,  -.2,0}, {.2, -.2,0},
-                             {-.1,-.3,0}, {.1, -.3,0}, {-.3,-.3,0},
-                             {.3, -.3,0}, {0,  -.4,0}, {0,  -.5,0},
-                           };
 /* Initialize alpha blending function */
 static void init(void) {
-    glEnable(GL_DEPTH_TEST);
-    printf("depth test enabled: %d\n", depth);
     glShadeModel(GL_FLAT);
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glPointSize(2);
 }
 
-void drawTree(void) {
-
-    glEnable (GL_POINT_SMOOTH);
-    glBegin(GL_POINTS);
-    for (int i = 0; i < count; ++i) {
-        glVertex3f(tree[i][0],tree[i][1],tree[i][2]);
-    }
-    glEnd();
-
-    glPushMatrix();
-    glBegin(GL_LINES); 
-        glVertex3f(0,-0.6,-0.9);
-        glVertex3f(tree[count][0],tree[count][1],tree[count][2]);
-    glEnd();
-    glPopMatrix();
-}
-
-void drawLaser(void) {
-
-}
 
 void timer(int value) {
     glutPostRedisplay();
+    /*
     if (count < (sizeof(tree) / sizeof(tree[0]))) {
         count++;
     } else {
         count = 0;
     }
+    */
     glutTimerFunc(250, timer, 0);  // re-register
 }
 
-void rect(GLfloat x, GLfloat y, GLfloat z, GLfloat w, GLfloat h, GLfloat d) {
+/* LINE SMOOTHING
     glEnable (GL_LINE_SMOOTH);
     glEnable (GL_BLEND);
     glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glHint (GL_LINE_SMOOTH_HINT, GL_NICEST);
+*/
 
-    glBegin( GL_LINE_LOOP );
-        glVertex3f(x,y,z);
-        glVertex3f(x+w,y,z);
-        glVertex3f(x+w,y+h,z);
-        glVertex3f(x,y+h,z);
-    glEnd();
-
-    glBegin( GL_LINE_LOOP );
-        glVertex3f(x,y,z+d);
-        glVertex3f(x+w,y,z+d);
-        glVertex3f(x+w,y+h,z+d);
-        glVertex3f(x,y+h,z+d);
-    glEnd();
-
-    glBegin(GL_LINES);
-        glVertex3f(x,y,z);
-        glVertex3f(x,y,z+d);
-    glEnd();
-
-    glBegin(GL_LINES);
-        glVertex3f(x+w,y,z);
-        glVertex3f(x+w,y,z+d);
-    glEnd();
-
-    glBegin(GL_LINES);
-        glVertex3f(x+w,y+h,z);
-        glVertex3f(x+w,y+h,z+d);
-    glEnd();
-        
-    glBegin(GL_LINES);
-        glVertex3f(x,y+h,z);
-        glVertex3f(x,y+h,z+d);
+void drawCircle(float radius) {
+    glBegin(GL_POLYGON);
+    for (int i=0; i < 360; i++) {
+        float degInRad = i*(3.14159/180);
+        glVertex2f(cos(degInRad)*radius,sin(degInRad)*radius);
+    }
     glEnd();
 }
+
 
 
 void display(void) {
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
+    /*
+    glEnable (GL_LINE_SMOOTH);
+    glEnable (GL_BLEND);
+    glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glHint (GL_LINE_SMOOTH_HINT, GL_NICEST);
+    */
+
+    
+    glColor3f(0.6,0.3,0.0);
+    glBegin(GL_QUADS);
+        glVertex2f(-1,0);
+        glVertex2f(1,0);
+        glVertex2f(1,1);
+        glVertex2f(-1,1);
+    glEnd();
+
+    glColor3f(1.0,1.0,1.0);
+    glBegin(GL_LINE_LOOP);
+        glVertex2f(-1,-1.1);
+        glVertex2f(1,-1.1);
+        glVertex2f(1,-0.1);
+        glVertex2f(-1,-0.1);
+    glEnd();
+
+    glColor3f(1.0,1.0,1.0);
     glPushMatrix();
-        glColor3f( 0.0, 1.0, 0.0 );
-        glRotatef(90, 0, 1, 0);
-        rect(-.4,-.4,0,.4,.4,.4);
-        glColor3f(1.0,0.0,0.0);
-        glTranslatef(-.2,-.1,.4);
-        glScalef(.5,.5,0);
-        drawTree();
-        glScalef(1,1,1);
+        glTranslatef(-.65,-.15,0);
+        glColor3f(0.2,1.0,.2);
+        glBegin(GL_TRIANGLES);
+            glVertex2f(0,0);
+            glVertex2f(-.3,-.3);
+            glVertex2f(.3,-.3);
+        glEnd();
     glPopMatrix();
 
-    glutSwapBuffers();
+    glPushMatrix();
+        glTranslatef(-.65,.95,0);
+        glColor3f(0.0,0.0,0.0);
+        glBegin(GL_TRIANGLES);
+            glVertex2f(0,0);
+            glVertex2f(-.3,-.3);
+            glVertex2f(.3,-.3);
+        glEnd();
+    glPopMatrix();
+
+    glPushMatrix();
+        glColor3f(0.0,0.0,0.0);
+        glTranslatef(0.0,.78,0.0);
+        drawCircle(0.18f);
+    glPopMatrix();
+
+    glPushMatrix();
+        glColor3f(1.0,0.0,0.0);
+        glTranslatef(0.0,-0.3,0.0);
+        drawCircle(0.18f);
+    glPopMatrix();
+
+
     glFlush();
 }
+
 
 void reshape(int w, int h) {
     glViewport(0, 0, (GLsizei) w, (GLsizei) h);
@@ -132,16 +136,6 @@ void reshape(int w, int h) {
 
 void keyboard(unsigned char key, int x, int y) {
     switch (key) {
-        case 'd':
-        case 'D':
-            depth = !depth;
-            if (depth) 
-                glEnable(GL_DEPTH_TEST);
-            else
-                glDisable(GL_DEPTH_TEST);
-            printf("depth test enabled: %d\n", depth);
-            glutPostRedisplay();
-            break;
         case 27: /* esc */
             exit(0);
             break;
@@ -152,7 +146,7 @@ void keyboard(unsigned char key, int x, int y) {
 
 int main(int argc, char* argv[]) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
     glutInitWindowSize(400, 400);
     glutCreateWindow(argv[0]);
     init();
