67cd89f70429734ae8dec4eb6614bb9b9d51fcaa
Brian Gianforcaro <bjg1955@rit.edu>
2009-01-22 02:04:51 -0500

    Change Summary: 	- Inital implementation of clip, not even sure it compiles.

--- project-two/clipPolygon.c
+++ project-two/clipPolygon.c
@@ -21,6 +21,47 @@
 void clipPolygon( GLint in, GLint inv[][2], GLint *out, GLint outv[][2],
 		  GLint x0, GLint y0, GLint x1, GLint y1 ) {
 
+	int inbounds(x,y) {
+		if ( (x >= x0 && x <= x1) && (y >= y0 && y <= y1)) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
+
+	void intersection (GLint, GLint, GLint, GLint, Glint out[2]) {
+
+	}
+	void output(GLint x, GLint y) {
+		outv[out][0] = x;
+		outv[out][1] = y;
+		out++;
+	}
+
+	int xS = inv[n - 1][0];
+	int yS = inv[n - 1][1];
+    int xP;
+	int yP;
+	for (int j = 0; j < in; j++) {
+		xP = inv[j][0];
+		yP = inv[j][1];
+		if (1 == inbounds(xP,yP)) { 
+			if (0 == inbounds (xS, yS)) {
+				GLint out[2];
+				intersection(xS,yS,xP,yP,out);
+				output(out[0],out[1]);
+			}
+			output(xP,yP);
+		} else if (1 == inbounds(xS,yS)) {
+			GLint out[2];
+			intersection(xP,yP,xS,yS,out);
+			output(out[0],out[1]);
+		}
+		xS = xP;
+		yS = yP;
+	}
+
 	/* your implementation here */
 
 }
+
