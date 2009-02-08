a1a563939b6561da555291af8dd2bb29d0f2f9ad
Brian Gianforcaro <b.gianfo@gmail.com>
2009-02-08 11:56:47 -0500

    more animation

--- project-three/project3.c
+++ project-three/project3.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <math.h>
 #if defined(__APPLE__) && defined(__MACH__)
 #	include <GLUT/glut.h>
 #else
@@ -9,7 +10,7 @@ const static int WIDTH = 800;
 const static int HEIGHT = 600;
 int window;
 int spin = 0;
-int animate = 1;
+int animate = 0;
 
 /**
  * Cordinate all scene drawing.
@@ -18,17 +19,35 @@ int animate = 1;
  */
 static void display ( void )
 {
-	glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-	glLoadIdentity();
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    glLoadIdentity();
 
-	glColor4f(1.0,0.3,0.3,0.0);
-	glTranslatef(0.0,0.0,-20.0);
-	glPushMatrix();
-		glRotatef(spin,1.0,1.0,0.0);
-		glutSolidCube(1);
-	glPopMatrix();
+    glColor4f(1.0,0.3,0.3,0.0);
+    glTranslatef(0.0,0.0,-30.0);
 
-	glutSwapBuffers();
+    glColor3f(1.0,0.0,0.0);
+    int radius = 10;
+    for (int i = 0; i < 360; i=i+15) {
+        float deginRad = i*(3.1459/180.0);
+        glPushMatrix();
+            glRotatef(spin,1.0,1.0,1.0);
+            glPushMatrix();
+                glTranslatef(cos(deginRad)*radius, sin(deginRad)*radius,0.0f);
+                glPushMatrix();
+                    glRotatef(-(spin),1.0,1.0,0.0);
+                    glutSolidCube(1);
+                glPopMatrix();
+            glPopMatrix();
+        glPopMatrix();
+    }
+
+
+    glColor3f(0.2,0.2,0.8);
+    glPushMatrix();
+        glRotatef(spin,1.0,1.0,0.0);
+        glutSolidIcosahedron();
+    glPopMatrix();
+    glutSwapBuffers();
 }
 
 /**
@@ -40,45 +59,45 @@ static void display ( void )
  */
 static void keyboard ( unsigned char key, int x, int y )
 {
-	switch ( key ) {
-		case 'q':
-		case 'Q':
-		case 033:
-			glutDestroyWindow(window);
-			exit( EXIT_SUCCESS );
-			break;
-		case '1':
-			//Select camera position 1
-			glMatrixMode(GL_PROJECTION); // set the view volume shape
-			glLoadIdentity();
-			glOrtho(-2.0*64/48.0, 2.0*64/48.0, -2.0, 2.0, 0.1, 100);
-			glMatrixMode(GL_MODELVIEW); // position and aim the camera
-			glLoadIdentity();
-			gluLookAt(2.0, 2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-			break;
-		case '2':
-			//Select camera position 2
-			glMatrixMode(GL_PROJECTION); // set the view volume shape
-			glLoadIdentity();
-			gluPerspective(45.0f,(GLfloat)WIDTH/(GLfloat)HEIGHT,0.1f,100.0f);
-			glMatrixMode(GL_MODELVIEW); // position and aim the camera
-			break;
-		case '5':
-			//Toggle first light source
-			(glIsEnabled(GL_LIGHT0) == GL_TRUE ) ? glDisable(GL_LIGHT0) : glEnable(GL_LIGHT0);
-			break;
-		case '6':
-			//Toggle second light source
-			(glIsEnabled(GL_LIGHT1) == GL_TRUE) ? glDisable(GL_LIGHT1) : glEnable(GL_LIGHT1);
-			break;
-		case 'a':
-			//Toggle animation state
-			animate = (animate == 1) ? 0 : 1;
-			break;
-		default:
-			glutPostRedisplay();
-			break;
-	}
+    switch ( key ) {
+        case 'q':
+        case 'Q':
+        case 033:
+                glutDestroyWindow(window);
+                exit( EXIT_SUCCESS );
+                break;
+        case '1':
+               //Select camera position 2
+                glMatrixMode(GL_PROJECTION); // set the view volume shape
+                glLoadIdentity();
+                gluPerspective(45.0f,(GLfloat)WIDTH/(GLfloat)HEIGHT,0.1f,100.0f);
+                glMatrixMode(GL_MODELVIEW); // position and aim the camera
+                break;
+        case '2':
+                //Select camera position 1
+                glMatrixMode(GL_PROJECTION); // set the view volume shape
+                glLoadIdentity();
+                glOrtho(-2.0*64/48.0, 2.0*64/48.0, -2.0, 2.0, 0.1, 100);
+                glMatrixMode(GL_MODELVIEW); // position and aim the camera
+                glLoadIdentity();
+                gluLookAt(2.0, 2.0, 2.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+                break;
+        case '5':
+                //Toggle first light source
+                (glIsEnabled(GL_LIGHT0) == GL_TRUE ) ? glDisable(GL_LIGHT0) : glEnable(GL_LIGHT0);
+                break;
+        case '6':
+                //Toggle second light source
+                (glIsEnabled(GL_LIGHT1) == GL_TRUE) ? glDisable(GL_LIGHT1) : glEnable(GL_LIGHT1);
+                break;
+        case 'a':
+                //Toggle animation state
+                animate = (animate == 1) ? 0 : 1;
+                break;
+        default:
+                glutPostRedisplay();
+                break;
+    }
 }
 
 /**
@@ -88,14 +107,13 @@ static void keyboard ( unsigned char key, int x, int y )
  */
 void timer(int timernum) 
 {
-	if (0 == timernum ) {
-		if (animate == 1) {
-
-			spin++;
-			glutPostRedisplay();
-		}
-		glutTimerFunc(15,timer,0);
-	}
+    if (0 == timernum ) {
+        if (animate == 1) {
+            spin++;
+            glutPostRedisplay();
+        }
+        glutTimerFunc(15,timer,0);
+    }
 }
 
 /**
@@ -105,24 +123,26 @@ void timer(int timernum)
  */
 static void init ( void )
 {
-	glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f );
-	glClearDepth( 1.0 );
-	glDepthFunc(GL_LESS);
-	glEnable(GL_DEPTH_TEST);
-	glShadeModel(GL_SMOOTH);
+    glClearColor ( 0.0f, 0.0f, 0.0f, 0.0f );
+    glClearDepth( 1.0 );
+    glDepthFunc(GL_LESS);
+    glEnable(GL_DEPTH_TEST);
+    glShadeModel(GL_SMOOTH);
 
-	glMatrixMode(GL_PROJECTION); // set the view volume shape
-  	glLoadIdentity();
-	gluPerspective(45.0f,(GLfloat)WIDTH/(GLfloat)HEIGHT,0.1f,100.0f);
-	glMatrixMode(GL_MODELVIEW); // position and aim the camera
+    glMatrixMode(GL_PROJECTION); // set the view volume shape
+    glLoadIdentity();
+    gluPerspective(45.0f,(GLfloat)WIDTH/(GLfloat)HEIGHT,0.1f,100.0f);
+    glMatrixMode(GL_MODELVIEW); // position and aim the camera
 
-	glEnable(GL_LIGHTING);
-	//glColorMaterial ( GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE ) ;
+    glEnable(GL_LIGHTING);
+    //glColorMaterial ( GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE ) ;
     glEnable ( GL_COLOR_MATERIAL ) ;
 
-	glEnable(GL_LIGHT0);
-	GLfloat light_pos[] = {3.0,3.0,3.0,0.0};
-	glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+    GLfloat light_pos[] = {3.0,3.0,3.0,0.0};
+    glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+
+    GLfloat light2_pos[] = {-3.0,-3.0,3.0,1.0};
+    glLightfv(GL_LIGHT2, GL_POSITION, light2_pos);
 }
 
 
@@ -135,7 +155,7 @@ static void init ( void )
 int main ( int argc, char** argv ) 
 {
 	glutInit( &argc , argv );
-	glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE );
+	glutInitDisplayMode( GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH );
 	glutInitWindowPosition( (glutGet(GLUT_SCREEN_WIDTH)/2)-(WIDTH/2), (glutGet(GLUT_SCREEN_HEIGHT)/2)-(HEIGHT/2));
 	glutInitWindowSize( WIDTH, HEIGHT );
 	window = glutCreateWindow( "Project #3" );
