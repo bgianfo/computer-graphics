d606c8b719630e0522c4f12f901077b70b039351
Brian Gianforcaro <bjg1955@rit.edu>
2009-02-11 05:04:22 -0500

    Got base gemotry up and correct

--- project-four/shape.cpp
+++ project-four/shape.cpp
@@ -32,12 +32,12 @@ void Shape::addTriangle(const Point3 &p1, const Point3 &p2, const Point3 &p3) {
 }
 
 Cube::Cube(int n) : Shape() {
-    const float DIM = 0.5;
-    const float R = (2*DIM)/n;
+    const double DIM = 0.5;
+    const double R = (2*DIM)/n;
     for ( int face = 0; face < 6; face++ ) {
-        float xval = -(DIM);
+        double xval = -(DIM);
         for ( int i = 0; i < n; i++ ) {
-            float yval = DIM;
+            double yval = DIM;
             for ( int j = 0; j < n; j++ ) {
                 Point3 p1; Point3 p2; Point3 p3; Point3 p4;
                 switch ( face ) {
@@ -138,9 +138,9 @@ Cylinder::Cylinder(int n, int m) : Shape() {
 		n = 3;
 
 	// Your code for tessellating a cylinder goes here
-	const static float MINY = -0.5;	
-	const static float MAXY = 0.5;	
-	const static float RAD = 0.5;
+	const static double MINY = -0.5;	
+	const static double MAXY = 0.5;	
+	const static double RAD = 0.5;
 	
 	const double YINC = (2*MAXY)/m;
 	for ( int i = 0; i < 360; i += 360/n ) {
@@ -171,10 +171,36 @@ Cylinder::Cylinder(int n, int m) : Shape() {
 }
 
 Sphere::Sphere(int n) : Shape() {
-
+#define T(a,b,c) addTriangle(a,b,c);
 	// Your code for tessellating a sphere goes here
+	if (n > 5) 
+		n = 5;
+
+	const static double A = 2/(1+sqrt(5));
+	const static double R = .5;
+
+	Point3 v0;Point3 v1;Point3 v2;Point3 v3;Point3 v4; Point3 v5;
+	Point3 v6;Point3 v7;Point3 v8;Point3 v9;Point3 v10;Point3 v11;
+	v0.x = 0; v0.y = A; v0.z =-R;    v1.x =-A; v1.y = R; v1.z = 0;
+	v2.x = A; v2.y = R; v2.z = 0;    v3.x = 0; v3.y = A; v3.z = R;
+	v4.x =-R; v4.y = 0; v4.z = A;    v5.x = 0; v5.y =-A; v5.z = R;
+	v6.x = R; v6.y = 0; v6.z = A;    v7.x = R; v7.y = 0; v7.z =-A;
+	v8.x = 0; v8.y =-A; v8.z =-R;    v9.x =-R; v9.y = 0; v9.z =-A;
+   v10.x =-A; v10.y=-R;v10.z = 0;   v11.x = A;v11.y =-R;v11.z = 0;
 
 
+    T(v0,v1,v2);  T(v3,v2,v1);
+    T(v3,v4,v5);  T(v3,v5,v6);
+    T(v0,v7,v8);  T(v0,v8,v9);
+    T(v5,v10,v11);T(v8,v11,v10);
+    T(v1,v9,v4);  T(v10,v4,v9);
+    T(v2,v6,v7);  T(v11,v7,v6);
+    T(v3,v1,v4);  T(v3,v6,v2);
+    T(v0,v9,v1);  T(v0,v2,v7);
+    T(v8,v10,v9); T(v8,v7,v11);
+    T(v5,v4,v10); T(v5,v11,v6);
+
+#undef T
 	// Note:
 	// If you decide to solve this problem by recursively subdividing
 	// a regular platonic solid, you may use the geometric
@@ -184,5 +210,9 @@ Sphere::Sphere(int n) : Shape() {
 	// You should divide each triangle into 4 smaller triangles as the
 	// parameter 'n' increases.  There is no need to go beyond 5
 	// levels of subdivision - i.e. if n > 5 then set n = 5
+
+}
+
+Sphere::Sphere(int n, const Point3 &p1, const Point3 &p2, const Point3 &p3) :Shape() {
 
 }
